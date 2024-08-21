181be3093d3329fe82c6f2f0f6d24a025c59bc0e
Sandy-390 <tennysonmaxx10@gmail.com>
2024-08-21 23:43:55 +0530

    it is py file

---
+++ qqq.py
@@ -0,0 +1,12 @@
+class train:
+    noofwheels = 100
+    seatt = 250
+    noofengines= 2
+
+    def forwarding():
+        print("train is stop")
+
+train1 = train()
+print(train1.noofwheels)
+print(train1.seatt)
+print(train1.noofengines)
